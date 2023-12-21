314742eb8fae2b68d762f7beb9c4101bca7e2f9d
diegoDevCodee <diegodevcodee@gmail.com>
2023-12-21 16:04:08 -0500

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CardComponent from "./components/CardComponent";
+
+function App() {
+  return (
+    <main className="bg-lightGray min-h-screen grid place-content-center p-4 sm:p-0">
+      <CardComponent />
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        whiteText: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+  },
+  plugins: [],
+};
